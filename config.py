b86b80f7b1d9aecc7e1b82b5990d68bd6720e7be
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2024-01-02 18:37:02 +1100

    data first values to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+REPO_PATH = os.getcwd().split('IR-LLM-for-Effort-Estimation')[0] + 'IR-LLM-for-Effort-Estimation'
+DATA_PATH = REPO_PATH + '/UQV100-and-180-selected-TREC-topics'
+UQV100_DATA_PATH = DATA_PATH + '/3180694'
+
+LOGS_PATH = REPO_PATH + '/logs'
+GRAPHICS_PATH = LOGS_PATH + '/graphics'
