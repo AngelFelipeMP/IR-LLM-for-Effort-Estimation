f31fea1ef69f13abdd72fb9640c23ab1f43c1e85
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2024-01-03 16:06:17 +1100

    label_distribution.py - draft 1

--- label_distribution.py
+++ label_distribution.py
@@ -1,17 +1,42 @@
 ##TODO: Load average label per backstory
-## -  get first one row per backstory/UQV100Id
-## -  add a new column with the rounded average label
+## [X]  get first one row per backstory/UQV100Id
+## [X]  add a new column with the rounded average label
 
 ##TODO: Load all labels
-## -  caculate the magority vote label
-## -  if there is a tie, sabe a list of labels
+## [X]  caculate the magority vote label
+## [X]  if there is a tie, sabe a list of labels
+
+##TODO: I STOPED HERE
+#  [X] I need to check if I need to Analyse the labels futher
+#  [X] add a column with the frequence of the most frequent label
+#  [X] add a coumn with the percentage of the most frequent label
+#  [X] I will discrabe the label distribution for each sample, 
+#       (X) standard deviation
+#       (X) Coefficient of Variation
+#       (X) Compare with Expected Values
+#  [X] save file with all the transformations (one row per UQV100Id)
+#  [X] save file with only Tied rows
+
+##TODO: Write a report
+#  [ ] write a report about the two possible gold labels
+#  [ ] Discus about the caculated parameters (SD, CV, etc)
+#  [ ] There is no sigle sample with the same label
+#  [ ] What means high Spread and low Spread - mention the intervals
+#  [ ] Should we Remove Tied Majority Vote samples?
+#  [ ] Write down numbers of samples with Tied Majority Vote
+#  [ ] Write down numbers of samples with 'NO_MATCH' -> Majority Vote and Average Labels
+#  [ ] Num high/Low/Medium spread samples -> based on CV and ASD
+#  [ ] print a histogram with the anottetion/distribution of labels for sample
+#  [ ] print a histogram with the distribution of labels for Majority Vote and Average Labels - together (I want to see the difference)
 
 ##TODO: Create a graphic with the distribution of labels
+#  [ ] I may change the name of the script to labels_analysis.py
 
 import pandas as pd
 from config import *
 from icecream import ic
 from collections import Counter
+import random
 
 class UQV100_QUERIES_AND_ESTIMATES:
     def __init__(self):
@@ -30,38 +55,55 @@ class UQV100_QUERIES_AND_ESTIMATES:
         self.df['DocCountAverageRound'] = self.df['DocCountAverageRound'].astype(int)
         
         self.df['DocCountList'] = self.df.groupby('UQV100Id')['DocCount'].apply(list)
+        
+        self.df['DocCountSD'] = self.df['DocCountList'].apply(lambda x: round(pd.Series(x).std(), 2))
+        
+        self.df['CoefficienOfVariation'] = self.df['DocCountList'].apply(lambda x: round(pd.Series(x).std() / pd.Series(x).mean(), 2))
+        ##NOTE: Discus this interval
+        self.df['DistSpreadBasedOnCV'] = self.df['CoefficienOfVariation'].apply(lambda x: 'Low' if x < 0.2 else 'Medium' if x < 0.4 else 'High')
+        
+        self.df['AverageSD'] = self.df.groupby('UQV100Id')['DocCountSD'].first().mean()
+        ##NOTE: Discus this interval
+        self.df['DistSpreadBasedOnASD'] = self.df['DocCountSD'].apply(lambda x: 'Low' if x < self.df['AverageSD'].iloc[0]*0.4 else 'Medium' if x < self.df['AverageSD'].iloc[0]*0.6 else 'High')
+        
         self.df['NumAnotettors'] = self.df.apply(lambda row: len(row['DocCountList']), axis=1)
-        self.df['DocMostFrequent'] = self.df['DocCountList'].apply(lambda x: [k for k, v in Counter(x).items() if v == max(Counter(x).values())])
         
-        self.df['DocCountTie'] = self.df['DocMostFrequent'].apply(lambda x: 'TIE' if len(x)>1 else 'NO_TIE')
+        self.df['DocCountMostFrequent'] = self.df['DocCountList'].apply(lambda x: [k for k, v in Counter(x).items() if v == max(Counter(x).values())])
         
-        self.df = self.df.loc[self.df['DocCountTie'] == 'TIE']
+        self.df['NumAnottetorsMostFrequent'] = self.df['DocCountList'].apply(lambda x: max(Counter(x).values()))
+        
+        self.df['PorcentageAnottetorsMostFrequent'] = round(self.df['NumAnottetorsMostFrequent'] / self.df['NumAnotettors'] * 100).astype(int)
+        
+        self.df['DocCountTie'] = self.df['DocCountMostFrequent'].apply(lambda x: 'TIE' if len(x)>1 else 'NO_TIE')
+        
+        self.df['MajorityVote'] = self.df['DocCountMostFrequent'].apply(lambda x: x[0] if len(x)==1  else random.choice(x))
+        
+        self.df['DistanceMVandAverage'] = abs(self.df['MajorityVote'] - self.df['DocCountAverageRound'])
+        
+        self.df['MatchMVandAverage'] = self.df['DistanceMVandAverage'].apply(lambda x: 'MATCH' if x == 0 else 'NO_MATCH')
+        
+        self.df['DocCountList'] =  self.df['DocCountList'].astype(str)
+        
         self.df = self.df.reset_index().drop_duplicates(subset='UQV100Id', keep='first').set_index('UQV100Id')
-        # self.df = self.df.drop_duplicates(subset='UQV100Id')
+
+        # ic(self.df)
+        # ic(len(self.df))
         
-        ##NOTE: I STOPED HERE
-        #       - the code is working
-        #       - I need to check if I need to Analyse the labels futher
-        #       - I may need to discrabe the label distribution for each sample, caculate the variance, standard deviation, etc.
-        #       - I should check the values which are not tied
-
-
-        ic(self.df)
-        ic(len(self.df))
+    def save_data(self):
+        # All backstories
+        self.df.to_csv(LOGS_PATH + '/' + 'uqv100-labels-analysis.tsv', sep='\t')
+        
+    def graphics(self):
+        pass
+    
+    def report(self):
+        pass
         
     def main(self):
         self.load_data()
         self.data_transformation()
+        self.save_data()
     
 if __name__ == '__main__':
     UQV100 = UQV100_QUERIES_AND_ESTIMATES()
     UQV100.main()
-    
-    
-    
-        # self.df_ = self.df.drop_duplicates(keep='first')
-        # self.df = self.df.drop('DocCount', axis=1)
-        
-        # ic(self.df.head())
-        
-                # self.df['MajorityVote'] = self.df['DocCountList'].apply(lambda x: max(set(x), key = x.count))
