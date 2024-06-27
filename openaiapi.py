91105cd756b1a53403e6a3a74def88e1b3be485b
Angel Felipe Magnossao de Paula <angel.magnossao@gmail.com>
2024-06-27 17:11:05 +1000

    mmls code for predicting effort estimation - draft 1

--- openaiapi.py
+++ openaiapi.py
@@ -13,22 +13,20 @@ class OpenAIAPI:
     def __init__(self, prompt):
         self.prompt = prompt
 
-    def get_completion(self, model="gpt-3.5-turbo-1106"):
+    def get_completion(self, model="gpt-3.5-turbo-0125"):
         try:
             # To prevent maintaining context, we keep only the necessary message
-            messages = [{"role": "user", "content": self.prompt}]
             response = client.chat.completions.create(
                 # response_format={ "type": "json_object" },
                 model=model,
                 messages=[
-                    {"role": "system", "content": "You are a helpful assistant designed to answer questions about the world."},
-                    {"role": "user", "content": "Who won the world series in 2020?"}],
+                    {"role": "system", "content": "You are an expert in predicting the number of necessary documents to attend to the user's needs."},
+                    {"role": "user", "content": self.prompt}],
                 temperature=0,  # this is the degree of randomness of the model's output
-                max_tokens = None, ##TODO: change this to 10
+                max_tokens = 10, ##TODO: change this to 10
                 top_p=1,
                 seed=42
             )
-            ic(response)
             return response.choices[0].message.content
         except Exception as e:
             logging.error(f"An error occurred during the OpenAI API call: {e}")
